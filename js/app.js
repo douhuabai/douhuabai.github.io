c1a93d8c7144a7d16fdee20a20e03a2033e27f15
douhuabai <douhuabai@outlook.com>
2015-02-25 23:03:35 +0800

    ..

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 window.addEventListener('load',function(){
-    alert(0)
+    var ps=document.querySelector('.markdown').querySelectorAll('p');
+    var i=0;
+    for(i=0;i<ps.length;i++){
+        var p=ps[i];
+        var txt=p.innerHTML;
+        if(txt.indexOf('<')==-1){
+           console.log(txt); 
+           var text='';
+            var ii=0;
+           for(ii=0;i<txt.length;ii++){
+               text+='';
+               text+=txt[ii];
+           }
+           p.innerHTML=text;
+        }
+    }
 });
